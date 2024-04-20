f930be13c45ae180e80c0be23788e2e55f7db506
Alin Abdallh <abdallh.alin10@gmail.com>
2024-04-20 16:27:21 +0200

    added coloring to the cards based on the pokemon types

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -4,6 +4,24 @@ import { Link } from 'react-router-dom';
 import './Pokedex.css'; 
 import PokeLogo from '../logo/PokeLogo.png';
 
+const typeColors = {
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: '#DEF3FD',
+  ground: '#f4e7da',
+  rock: '#d5d5d4',
+  fairy: '#fceaff',
+  poison: '#98d7a5',
+  bug: '#f8d5a3',
+  dragon: '#97b3e6',
+  psychic: '#eaeda1',
+  flying: '#F5F5F5',
+  fighting: '#E6E0D4',
+  normal: '#F5F5F5',
+};
+
+
 const Pokedex = () => {
   const [pokemons, setPokemons] = useState([]);
   const [offset, setOffset] = useState(0);
@@ -12,11 +30,21 @@ const Pokedex = () => {
 
   useEffect(() => {
     axios.get(currentPageUrl).then(res => {
-      const fetchedPokemons = res.data.results;
-      const pokemonsWithImages = fetchedPokemons.map(pokemon => {
-        return { ...pokemon, imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png` };
+      const promises = res.data.results.map(pokemon => {
+        return axios.get(pokemon.url).then(pokemonRes => {
+          const types = pokemonRes.data.types.map(t => t.type.name);
+          const typeColor = types[0] ? typeColors[types[0]] : '#F5F5F5';
+          return {
+            ...pokemon,
+            imageUrl: pokemonRes.data.sprites.front_default,
+            types,
+            typeColor
+          };
+        });
       });
-      setPokemons(pokemonsWithImages);
+      Promise.all(promises).then(pokemonsData => {
+        setPokemons(pokemonsData);
+      });
     });
   }, [currentPageUrl]);
 
@@ -37,16 +65,24 @@ const Pokedex = () => {
           className="pokemon-logo"
         />
       </header>
-      <div className="pokedex-grid">
-        {pokemons.map(pokemon => (
-          <div className="card" key={pokemon.name}>
-            <Link to={`/${pokemon.name}`}>
-              <img src={pokemon.imageUrl} alt={pokemon.name} className="pokemon-image" />
-              <h2 className="pokemon-name">{pokemon.name}</h2>
-            </Link>
-          </div>
-        ))}
+      <div className="navigation-buttons">
+        <button onClick={goToPreviousPage} disabled={offset === 0}>Previous Page</button>
+        <button onClick={goToNextPage}>Next Page</button>
       </div>
+      <div className="pokedex-grid">
+      {pokemons.map(pokemon => (
+        <div 
+          className="card" 
+          key={pokemon.name} 
+          style={{ backgroundColor: pokemon.typeColor }} 
+        >
+          <Link to={`/${pokemon.name}`}>
+            <img src={pokemon.imageUrl} alt={pokemon.name} className="pokemon-image" />
+            <h2 className="pokemon-name">{pokemon.name}</h2>
+          </Link>
+        </div>
+      ))}
+    </div>
       <div className="navigation-buttons">
         <button onClick={goToPreviousPage} disabled={offset === 0}>Previous Page</button>
         <button onClick={goToNextPage}>Next Page</button>
--- src/components/Root.js
+++ src/components/Root.js
@@ -2,11 +2,20 @@ import "../index.css"
 import { Link, Outlet } from "react-router-dom"
 
 export default function App() {
+    const linkStyle = {
+        padding: "10px",
+        textDecoration: "none",
+        color: "black",
+        backgroundColor: "#ffcc00",
+        borderRadius: "5px",
+        margin: "5px",
+        fontWeight: "bold",
+    };
     return (
         <>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
+            <nav style={{ backgroundColor: "#ffcb05", padding: "10px" }}>
+                <Link to="/" style={linkStyle}>Home</Link>
+                <Link to="/about" style={linkStyle}>About</Link>
             </nav>
             <Outlet />
         </>
