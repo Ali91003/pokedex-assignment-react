83af7079ee4e057a86bae206cc25b29219f64d8e
Alin Abdallh <abdallh.alin10@gmail.com>
2024-04-20 17:17:37 +0200

    added styling to the PokemonDetails component

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import './PokemonDetails.css';
+import PokeLogo from '../logo/PokeLogo.png';
 
 const PokemonDetails = () => {
   const { pokemonName } = useParams();
@@ -13,16 +15,39 @@ const PokemonDetails = () => {
   }, [pokemonName]);
 
   return (
-    <div>
+    <div className="pokemon-details-container">
+    <header className="pokedex-header">
+      <img 
+        src={PokeLogo}
+        alt="Pokémon"
+        className="pokemon-logo"
+      />
+    </header>
+    
       {pokemonDetails ? (
-        <div>
+        <div className="pokemon-details">
           <h1>{pokemonDetails.name}</h1>
-          <img src={pokemonDetails.sprites.front_default} alt={pokemonDetails.name} />
-          <p>Type: {pokemonDetails.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
-          <p>Height: {pokemonDetails.height}</p>
-          <p>Weight: {pokemonDetails.weight}</p>
-          <p>Abilities: {pokemonDetails.abilities.map(abilityInfo => abilityInfo.ability.name).join(', ')}</p>
-        </div>
+          <img 
+            src={pokemonDetails.sprites.front_default} 
+            alt={pokemonDetails.name} 
+            className="pokemon-image" 
+          />
+          <div className="stat-item">
+            <span className="stat-name">Type:</span>
+            {pokemonDetails.types.map(typeInfo => typeInfo.type.name).join(', ')}
+          </div>
+          <div className="stat-item">
+            <span className="stat-name">Height:</span> {pokemonDetails.height}
+          </div>
+          <div className="stat-item">
+            <span className="stat-name">Weight:</span> {pokemonDetails.weight}
+          </div>
+          <div className="stat-item">
+            <span className="stat-name">Abilities:</span>
+            {pokemonDetails.abilities.map(abilityInfo => abilityInfo.ability.name).join(', ')}
+          </div>
+       </div>
+
       ) : (
         <p>Loading...</p>
       )}
